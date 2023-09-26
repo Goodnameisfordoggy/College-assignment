eddf03c8e4d15bd2387f9c177062c020aee483ee
Goodnameisfordoggy <3264310995@qq.com>
2023-09-26 06:31:01 +0000

    上传文件
    Signed-off-by: Goodnameisfordoggy <3264310995@qq.com>

---
+++ c BasicHomework/01章 简单程序设计/0103两个整数相加.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, ad;
+    scanf("%d%d", &a, &b);
+    ad = a + b;
+    printf("%d", ad);
+    return 0;
+}
---
+++ c BasicHomework/01章 简单程序设计/L0102求两个整数之和.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a = 12345;
+    int b = 54321;
+    int sum;
+    sum = a + b;
+    printf("sum is %d", sum);
+    return 0;
+}
